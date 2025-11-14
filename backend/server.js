44886f65eec06523307bd01d313425d3fe1a94ba
radha <dev.ranamanvi@gmail.com>
2025-11-14 22:37:15 +0530

    Initial setup: Added backend Express server and Next.js frontend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
